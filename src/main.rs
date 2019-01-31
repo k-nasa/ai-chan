18a8ee110000901761a6681fef6b40c9864b8028
nasa <htilcs1115@gmail.com>
2019-01-31 17:46:33 +0900

    Add hello test

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-use rocket::{get, routes};
+
+use rocket::{get, post, routes, Data};
+use std::io::Read;
 
 #[get("/")]
 fn index() -> &'static str {
+    // 使い方を出すように変更
     "Hello, world!"
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket().launch();
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![index, github])
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rocket::http::Status;
+    use rocket::local::Client;
+
+    #[test]
+    fn test_index() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+
+        let mut response = client.get("/").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.body_string(), Some("Hello, world!".into()));
+    }
 }
