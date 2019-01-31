43ae849c814df8cef3569efc46705e04c17f3b71
nasa <htilcs1115@gmail.com>
2019-01-31 19:14:24 +0900

    test: Add GitHubEvent::from_request() test

--- src/hooks.rs
+++ src/hooks.rs
@@ -4,3 +4,25 @@ use rocket::request::{FromRequest, Outcome, Request};
 pub enum GitHubEvent {
     IssueComment,
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::*;
+    use rocket::http::Header;
+    use rocket::http::Status;
+    use rocket::local::Client;
+
+    #[test]
+    fn test_from_request() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+
+        let header = Header::new("X-GitHub-Event", "issue_comment");
+        let request = client.post("/").header(header).body("test");
+
+        let event = GitHubEvent::from_request(&request.inner());
+
+        assert!(event.is_success());
+        assert_eq!(event.unwrap(), GitHubEvent::IssueComment);
+    }
+}
