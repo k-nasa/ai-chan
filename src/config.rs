074d988c37ae2d880be9ba90cd2aba5e43c6ddd7
nasa <htilcs1115@gmail.com>
2019-02-01 21:56:24 +0900

    feat: Add Config::load_config

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,7 @@
 use serde_derive::*;
+use std::fs::*;
+use std::io::Read;
+use std::path::Path;
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct Config {
@@ -6,4 +9,28 @@ pub struct Config {
     address: String,
     botname: String,
     github_api_key: String,
+}
+
+impl Config {
+    pub fn load_config() -> Result<Self, failure::Error> {
+        let file_path = Path::new(&home_dir_string()?).join(".config/ai-chan/config.toml");
+        let mut file = match File::open(file_path) {
+            Ok(c) => c,
+            Err(_) => failure::bail!("Please create a file '~/.config/ai-chan/config.toml'"),
+        };
+
+        let mut toml_string = String::new();
+        file.read_to_string(&mut toml_string)?;
+
+        let config: Self = toml::from_str(&toml_string)?;
+
+        Ok(config)
+    }
+}
+
+fn home_dir_string() -> Result<String, failure::Error> {
+    match dirs::home_dir() {
+        Some(dir) => Ok(dir.to_str().unwrap().to_owned()),
+        _ => failure::bail!("Failed get home directory"),
+    }
 }
