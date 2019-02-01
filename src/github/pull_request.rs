c19a7456882f394d36fda2129891cdb9e404e5a6
nasa <htilcs1115@gmail.com>
2019-02-01 12:47:21 +0900

    test: Add PullRequestEvent deserialize test

--- src/github/pull_request.rs
+++ src/github/pull_request.rs
@@ -12,3 +12,31 @@ enum PullRequestAction {}
 
 #[derive(Deserialize, PartialEq, Debug)]
 pub struct PullRequest {}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::test_support::*;
+
+    #[test]
+    fn from_json_str() {
+        let event1: PullRequestEvent =
+            serde_json::from_str(&pull_request_webhook_payload()).unwrap();
+
+        let event2 = PullRequestEvent {
+            action: PullRequestAction::Closed,
+            number: 1,
+            pull_request: PullRequest {
+                id: 1,
+                url: "https://api.github.com/repos/Codertocat/Hello-World/pulls/1".to_owned(),
+                number: 1,
+                state: PullRequestState::Closed,
+                locked: false,
+                title: "Update the README with new information".to_owned(),
+                body: "This is a pretty simple change that we need to pull into master.".to_owned(),
+            },
+        };
+
+        assert_eq!(event1, event2);
+    }
+}
