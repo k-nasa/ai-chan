6975feae2c16ffab90516acc277c91292d42ea28
nasa <htilcs1115@gmail.com>
2019-02-02 11:09:20 +0900

    refactor: Restrict action type

--- src/request_handle/handle_pull_request.rs
+++ src/request_handle/handle_pull_request.rs
@@ -1,12 +1,21 @@
 use super::parse_command;
 use crate::config::Config;
-use crate::github::pull_request::PullRequestEvent;
+use crate::github::pull_request::{PullRequestAction, PullRequestEvent};
 use crate::AIChannResult;
 use hubcaps::*;
 use tokio::runtime::Runtime;
 
 pub fn exec(json: serde_json::Value) -> AIChannResult {
     let pull_request_event: PullRequestEvent = serde_json::from_value(json)?;
+
+    if pull_request_event.action != PullRequestAction::Opened {
+        warn!(
+            "Unsupport pull_request {:?} action",
+            pull_request_event.action
+        );
+
+        return Ok(());
+    }
 
     let command = parse_command(&pull_request_event.pull_request.body)?;
     let user_assign = command.user_assign();
