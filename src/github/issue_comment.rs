8865a36af937badbeb40f78de3a1352deef4c634
nasa <htilcs1115@gmail.com>
2019-02-02 10:20:27 +0900

    test: Add IssueCommentEvent::from_json test code

--- src/github/issue_comment.rs
+++ src/github/issue_comment.rs
@@ -24,3 +24,40 @@ pub struct Issue {}
 
 #[derive(Deserialize, Debug, PartialEq)]
 pub struct Comment {}
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::test_support::*;
+
+    #[test]
+    fn from_json_str() {
+        let event1: IssueCommentEvent = serde_json::from_str(&issue_comment_payload()).unwrap();
+
+        let event2 = IssueCommentEvent {
+            action: IssueCommentAction::Created,
+            issue: Issue {
+                id: 327_883_527,
+                url: "https://api.github.com/repos/Codertocat/Hello-World/issues/2".to_owned(),
+                number: 2,
+                title: "Spelling error in the README file".to_owned(),
+
+                body: "It looks like you accidently spelled 'commit' with two 't's.".to_owned(),
+            },
+            comment: Comment {
+                id: 393_304_133,
+
+                issue_url: "https://api.github.com/repos/Codertocat/Hello-World/issues/2"
+                    .to_owned(),
+
+                body: "You are totally right! I'll get this fixed right away.".to_owned(),
+            },
+            repository: Repository {
+                id: 135_493_233,
+                name: "Hello-World".to_owned(),
+                full_name: "Codertocat/Hello-World".to_owned(),
+            },
+        };
+
+        assert_eq!(event1, event2);
+    }
+}
